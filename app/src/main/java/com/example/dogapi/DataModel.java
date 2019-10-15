39d5fc94be5f2d9e134f09da4025eba546cdf6a7
ranjitha <ranjitharanju8@gmail.com>
2019-10-15 17:41:54 +0530

    data model

--- app/src/main/java/com/example/dogapi/DataModel.java
+++ app/src/main/java/com/example/dogapi/DataModel.java
@@ -12,27 +12,5 @@ public class DataModel {
         this.imgURL = imgURL;
     }
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getCountry() {
-//        return country;
-//    }
-//
-//    public void setCountry(String country) {
-//        this.country = country;
-//    }
-//
-//    public String getCity() {
-//        return city;
-//    }
-//
-//    public void setCity(String city) {
-//        this.city = city;
-//    }
+
 }
